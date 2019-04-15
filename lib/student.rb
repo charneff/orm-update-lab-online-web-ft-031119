6ef260b426519cac1f0670c91418292518e201c6
Charlotte Neff <charlotte.neff@gmail.com>
2019-04-15 14:45:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -21,9 +21,11 @@ class Student
 
      DB[:conn].execute(sql)
   end
+  
+  def self.drop_table
+    sql = <<-SQL
+    
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
-
+ 
 
 end
