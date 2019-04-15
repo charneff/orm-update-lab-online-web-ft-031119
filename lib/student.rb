de151e684447787dc7f076e3b0a2a611d53459c8
Charlotte Neff <charlotte.neff@gmail.com>
2019-04-15 14:42:37 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,7 +12,7 @@ class Student
   
   def self.create_table 
     sql = <<-SQL 
-    CREATE 
+    CREATE TABLE IF NOT EXISTS
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
